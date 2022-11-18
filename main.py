125ca617cd6838832921b62e670b2b0b3d5a60f9
conarin <53005059+conarin@users.noreply.github.com>
2022-11-18 16:11:11 +0900

    test

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('Hello')
+import os
+
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+
+client = MyClient()
+client.run(os.environ['TOKEN'])
